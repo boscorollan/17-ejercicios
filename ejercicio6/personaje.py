fa1791a28e050de1b2ebecf7b9ce60df616f0f01
boscorollan <bosco.rollanagudo@usp.ceu.es>
2025-09-29 18:06:39 +0000

    ej6

---
+++ ejercicio6/personaje.py
@@ -0,0 +1,55 @@
+#enunciado:
+#Una persona tiene un nombre, dos apellidos, una fecha de nacimiento, un sexo y un número de identificación.
+#Define las clases y los atributos correspondientes
+
+class Persona:
+    def __init__(self,nombre, apellido1, apellido2, fecha_nacimiento, sexo, num_id):
+        self.__nombre = nombre
+        self.__apellido1 = apellido1
+        self.__apellido2 = apellido2
+        self.__fecha_nacimiento = fecha_nacimiento
+        self.__sexo = sexo
+        self.__num_id = num_id
+
+    #GETTERS
+    def get_nombre(self):   
+            return self.__nombre
+    def get_apellido1(self):
+            return self.__apellido1
+    def get_apellido2(self):
+            return self.__apellido2
+    def get_fecha_nacimiento(self):
+            return self.__fecha_nacimiento
+    def get_sexo(self):
+            return self.__sexo
+    def get_num_id(self):
+            return self.__num_id
+        
+    #SETTERS
+    def set_nombre(self,nombre):
+            self.__nombre = nombre
+    def set_apellido1(self,apellido1):
+            self.__apellido1 = apellido1
+    def set_apellido2(self,apellido2):
+            self.__apellido2 = apellido2
+    def set_fecha_nacimiento(self,fecha_nacimiento):
+            self.__fecha_nacimiento = fecha_nacimiento
+    def set_sexo(self,sexo):
+            self.__sexo = sexo
+    def set_num_id(self,num_id):
+            self.__num_id = num_id
+
+
+    #comportamientos o metodos (funciones)
+    def saludar(self):
+        return f"Hola, me llamo {self.__nombre} {self.__apellido1} {self.__apellido2}, encantado."
+    def despedirse(self,otra_persona):
+        return f"Adiós {otra_persona}, ha sido un placer conocerte."
+    def cumpleaños(self):
+        return f"Feliz cumpleaños {self.__nombre} {self.__apellido1} {self.__apellido2}!"
+           
+
+
+    #representacion en cadena
+    def __str__(self):
+        return f"{self.__nombre} se apellida {self.__apellido1} {self.__apellido2}, su fecha de nacimiento es: {self.__fecha_nacimiento}, Sexo: {self.__sexo}, ID: {self.__num_id}"
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #aqui vamos a correr todos los ejercicios
+
+#ejercicio 6
+from ejercicio6.personaje import Persona  
+
+if __name__ == "__main__":
+    persona1 = Persona("Juan", "Gonzalez", "Heredia", "01/01/1990", "M", "12345678A")
+    yo = Persona("Bosco", "Rollán", "Agudo", "20/07/2006", "M", "87654321B")
+    print(yo)
+    print(persona1)
+    print(yo.saludar())
+    print(yo.despedirse("Ana"))
+
