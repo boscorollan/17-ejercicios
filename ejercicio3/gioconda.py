5ec95b6cb1c457960da69ef5a7bfab67081cc0b1
boscorollan <bosco.rollanagudo@usp.ceu.es>
2025-09-30 17:25:46 +0000

    2 giocondas

---
+++ ejercicio3/gioconda.py
@@ -0,0 +1,14 @@
+from ejercicio3.alpha import Cuadro
+
+la_gioconda = Cuadro(
+    titulo="La Gioconda",
+    adscripcion_cronologica="1503 - 1516",
+    tecnica="Óleo",
+    subtecnica="Sfumato",
+    soporte="Madera de álamo",
+    autor="Leonardo da Vinci",
+    estado_conservacion="Regular",
+    institucion="Museo del Louvre",
+    ciudad="París",
+    pais="Francia"
+)
---
+++ ejercicio3/gioconda_prado.py
@@ -0,0 +1,14 @@
+from ejercicio3.alpha import Cuadro
+gioconda_prado = Cuadro(
+    titulo="Gioconda de El Prado",
+    adscripcion_cronologica="1503 - 1516",
+    tecnica="Óleo",
+    subtecnica="Pincelada simple",
+    soporte="Madera de nogal",
+    autor="Desconocido",
+    estado_conservacion="Bueno",
+    institucion="Museo de El Prado",
+    ciudad="Madrid",
+    pais="España",
+    replica_de=la_gioconda
+)
