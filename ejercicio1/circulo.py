b4f2f733958b605b780acc1e75cbe04fa15c6c7e
boscorollan <bosco.rollanagudo@usp.ceu.es>
2025-09-30 13:45:49 +0000

    circulo

---
+++ ejercicio1/circulo.py
@@ -0,0 +1,15 @@
+from figura import Figura
+import math
+
+
+
+class Circulo(Figura):
+    def __init__(self, radio):
+        super().__init__()
+        self.radio = radio
+
+    def area(self):
+        return math.pi * self.radio ** 2
+
+    def perimetro(self):
+        return 2 * math.pi * self.radio
